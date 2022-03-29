00880b12a0a40455e08b3964e31d0b0229a27dd5
BjoernTroldahl <72730043+BjoernTroldahl@users.noreply.github.com>
2022-03-29 14:31:24 +0200

    Made everything workable

---
+++ Source/PluginEditor.h
@@ -0,0 +1,38 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+using namespace juce;
+//==============================================================================
+/**
+*/
+class RandomNameAudioProcessorEditor  : public juce::AudioProcessorEditor, public Timer
+{
+public:
+    RandomNameAudioProcessorEditor (RandomNameAudioProcessor&);
+    ~RandomNameAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+    Label title;
+    void configGUI();
+    TextButton newGame;
+    void timerCallback();
+    int frameRate = 30;
+    bool isPageChanged = false;
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    RandomNameAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (RandomNameAudioProcessorEditor)
+};
